d669b7093f72db707b854173ab9bf4518eff4824
Noybs125 <129349581+Noybs125@users.noreply.github.com>
2026-03-04 00:25:16 -0600

    added hub poses to field

--- 2026Robot/src/main/java/team7111/robot/subsystems/Field.java
+++ 2026Robot/src/main/java/team7111/robot/subsystems/Field.java
@@ -3,6 +3,8 @@ package team7111.robot.subsystems;
 import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Field extends SubsystemBase {
@@ -27,5 +29,15 @@ public class Field extends SubsystemBase {
 
     public boolean inNeutralZone(Pose2d pose){
         return !inAllianceZone(pose, true) && !inAllianceZone(pose, false);
+    }
+
+    public Pose2d getHub(boolean isRedAlliance){
+        return isRedAlliance
+            ? new Pose2d( 4.635, 4.039, Rotation2d.kZero)
+            : new Pose2d(11.946, 4.039, Rotation2d.kZero);
+    }
+
+    public Pose2d getAllianceHub(){
+        return getHub(isRedAlliance.getAsBoolean());
     }
 }
