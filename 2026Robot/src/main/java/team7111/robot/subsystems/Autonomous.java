2963f0d2ec1ff7110904769416998058b2f2f907
Firereader06 <benjjackson05@outlook.com>
2026-03-10 18:06:22 -0500

    ....

--- 2026Robot/src/main/java/team7111/robot/subsystems/Autonomous.java
+++ 2026Robot/src/main/java/team7111/robot/subsystems/Autonomous.java
@@ -59,8 +59,8 @@ public class Autonomous extends SubsystemBase {
         new Pose2d(2.996, 5.946, Rotation2d.fromDegrees(-45.65)),
         new Pose2d(2.267, 4.021, Rotation2d.fromDegrees(0)),
         new Pose2d(2.996, 2.096, Rotation2d.fromDegrees(52.65)),
-        new Pose2d(0.85, 7.5, Rotation2d.fromDegrees(-46)),
-        new Pose2d(0.85, 0.66, Rotation2d.fromDegrees(46)),
+        new Pose2d(0.85, 7, Rotation2d.fromDegrees(-46)),
+        new Pose2d(0.85, 1.16, Rotation2d.fromDegrees(46)),
     };
     //R = Right, L = Left, I = Intake, N = Neutral zone, A = alliance zone
      
